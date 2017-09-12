cce235d6fe5cd18ba1d14df16c16488e40db66a6
MrCliff <j_1.kallio@hotmail.com>
2017-09-12 22:34:40 +0300

    Created all the classes and started to work on the bottom ones first.

---
+++ Jalokivileikkuri/CutProduct.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Jalokivileikkuri
+{
+    /// <summary>
+    /// The product of a gem cut.
+    /// </summary>
+    class CutProduct
+    {
+        ICollection<Cut> cuts;
+
+
+        /// <summary>
+        /// The size of the original raw gem chunk.
+        /// </summary>
+        private int RawChunkSize { get; set; }
+
+
+        /// <summary>
+        /// The collection of the cuts that should be applied to the RawChunk.
+        /// </summary>
+        private ICollection<Cut> Cuts
+        {
+            get { return cuts; }
+            set { cuts = value; }
+        }
+
+
+        /// <summary>
+        /// The total value of all the cuts.
+        /// </summary>
+        private int Value
+        {
+            get { return 0; }
+        }
+
+
+        /// <summary>
+        /// The total waste left after all the cuts.
+        /// </summary>
+        private int Waste
+        {
+            get { return 0; }
+        }
+
+
+        /// <summary>
+        /// The total profit after all the cuts.
+        /// </summary>
+        private int Profit
+        {
+            get { return Value - Waste; }
+        }
+
+
+        /// <summary>
+        /// Initializes a new CutProduct with the given values.
+        /// </summary>
+        /// <param name="rawChunkSize">The size of the original raw gem chunk.</param>
+        /// <param name="cuts">The collection of the cuts that should be applied to the RawChunk.</param>
+        public CutProduct(int rawChunkSize, ICollection<Cut> cuts)
+        {
+            RawChunkSize = rawChunkSize;
+            Cuts = cuts;
+        }
+    }
+}
